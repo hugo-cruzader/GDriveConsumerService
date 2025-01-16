8dc368ce89090e68d160efc4fe12759bc57af6e1
Hugo Huipet <hugo.huipet@gmail.com>
2025-01-15 16:12:38 -0800

    Added minimal files for SpringBoot Project Startup

---
+++ src/test/java/com/hector/gdriveconsumerservice/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hector.gdriveconsumerservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class DemoApplicationTests {
+    @Test
+    void contextLoads() {
+        assertEquals(1, 1);
+    }
+}
