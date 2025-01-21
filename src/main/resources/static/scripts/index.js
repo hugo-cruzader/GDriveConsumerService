2703f25e6d20e9708d26598fac61a92497333541
Hugo Huipet <hugo.huipet@gmail.com>
2025-01-20 16:23:41 -0800

    feat: Replace Menu option for Table that loads on Dashboard ready function for List Files from G Drive

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -32,33 +32,57 @@ $(document).ready(function(){
         }
     });
 
-    $("#refreshFileButton").click(function(){
-        $.ajax({
-            url: '/listfolder',
-            }).done(function(data) {
-            console.dir(data);
-            var fileHTML = "";
-            for(file of data) {
-                var isFolder = file.type.includes("folder");
-                var buttonDownload = "";
-                var buttonDelete = "";
-                if (!isFolder) {
-                    buttonDownload = '<button class="btn btn-success" onclick="downloadFile(\''+ file.id + '\')">Download</button>'
-                    buttonDelete = '<button class="btn btn-danger" onclick="deleteFile(\''+ file.id + '\')">Delete</button>'
-                }
-                fileHTML += '<ul class="list-group list-group-horizontal" >';
-                fileHTML += '<li class="list-group-item">' + file.name + '</li>'
-                fileHTML += '<li class="list-group-item">' + file.type + '</li>'
-                fileHTML += '<li class="list-group-item">' + new Date(file.lastModifiedDate.value).toDateString() + '</li>'
-                fileHTML += '<li class="list-group-item">'+ buttonDownload + '</li>';
-                fileHTML += '<li class="list-group-item">'+ buttonDelete + '</li>';
-                fileHTML += '</ul>';
-            }
-            $("#fileListLayout").html(fileHTML);
-        });
+    $("#refreshButton").click(function(){
+        window.location.href = 'http://localhost:8080/';
     });
+
+    $.ajax({
+        url: '/listfolder',
+        }).done(function(data) {
+        console.dir(data);
+        var list_files =[];
+        var list_folders = []
+        for(file of data) {
+            var isFolder = file.type.includes("folder");
+            var element = {id: file.id, name: file.name, type: file.type, lastMod:new Date(file.lastModifiedDate.value).toDateString()};
+            if (isFolder){
+                list_folders.push(element);
+            } else {
+                list_files.push(element);
+            }
+        }
+
+        var folderRows = "";
+        list_folders.forEach( folder => {
+            folderRows += '<tr>';
+            folderRows += createCol(folder.name);
+            folderRows += createCol(folder.type);
+            folderRows += createCol(folder.lastMod);
+            folderRows += createCol("");
+            folderRows += createCol("");
+            folderRows += '</tr>';
+        });
+        $("#fileContainer").html($("#fileContainer").html() + folderRows);
+
+        var fileRows = "";
+        list_files.forEach( file => {
+            fileRows += '<tr>';
+            fileRows += createCol(file.name);
+            fileRows += createCol(file.type);
+            fileRows += createCol(file.lastMod);
+            fileRows += createCol('<button class="btn btn-success" onclick="downloadFile(\''+ file.id + '\')">Download</button>');
+            fileRows += createCol('<button class="btn btn-danger" onclick="deleteFile(\''+ file.id + '\')">Delete</button>');
+            fileRows += '</tr>';
+        });
+
+        $("#fileContainer").html($("#fileContainer").html() + fileRows);
+    });
+
 });
 
+function createCol(value) {
+    return '<td>' + value + '</td>';
+}
 
 function deleteFile(fileId) {
     $.ajax({
